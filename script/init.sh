48aead176625d2311342a30331b4bde6d071a871
h13i32maru <h13i32maru@gmail.com>
2015-02-01 01:54:09 +0900

    chore: check curl command.

--- script/init.sh
+++ script/init.sh
@@ -3,6 +3,10 @@
 npm install
 bower install
 
-mkdir -p misc/type
-curl --output misc/type/lib.es6.d.ts https://raw.githubusercontent.com/Microsoft/TypeScript/master/bin/lib.es6.d.ts
-curl --output misc/type/lib.dom.d.ts https://raw.githubusercontent.com/Microsoft/TypeScript/master/bin/lib.dom.d.ts
+type curl > /dev/null 2>&1
+if [ $? -eq 0 ]
+then
+    mkdir -p misc/type
+    curl --output misc/type/lib.es6.d.ts https://raw.githubusercontent.com/Microsoft/TypeScript/master/bin/lib.es6.d.ts 2>/dev/null
+    curl --output misc/type/lib.dom.d.ts https://raw.githubusercontent.com/Microsoft/TypeScript/master/bin/lib.dom.d.ts 2>/dev/null
+fi
